9e90c27daed64b8bfb8067f89de342aa356a6739
lndsilva <lndsilva@hotmail.com>
2024-08-23 19:43:02 -0300

    sql

---
+++ dbprodutos.sql
@@ -0,0 +1,55 @@
+drop database dbprodutos;
+
+create database dbprodutos;
+
+use dbprodutos;
+
+
+CREATE TABLE PRODUTOS 
+( 
+CODIGO INT, 
+NOME VARCHAR(50), 
+TIPO VARCHAR(25), 
+QUANTIDADE INT, 
+VALOR DECIMAL(10,2) 
+);
+ 
+INSERT INTO PRODUTOS ( CODIGO, NOME, TIPO, QUANTIDADE, VALOR ) VALUES ( 1,'IMPRESSORA', 'INFORMATICA', 200, 600.00 ); 
+INSERT INTO PRODUTOS ( CODIGO, NOME, TIPO, QUANTIDADE, VALOR ) VALUES ( 2,'CAMERA DIGITAL', 'DIGITAIS', 300, 400.00 ); 
+INSERT INTO PRODUTOS ( CODIGO, NOME, TIPO, QUANTIDADE, VALOR ) VALUES ( 3,'DVD PLAYER', 'ELETRONICOS', 250, 500.00 ); 
+INSERT INTO PRODUTOS ( CODIGO, NOME, TIPO, QUANTIDADE, VALOR ) VALUES ( 4,'MONITOR', 'INFORMATICA', 400, 900.00 ); 
+INSERT INTO PRODUTOS ( CODIGO, NOME, TIPO, QUANTIDADE, VALOR ) VALUES ( 5,'TELEVISOR', 'ELETRONICOS', 350, 650.00 ); 
+INSERT INTO PRODUTOS ( CODIGO, NOME, TIPO, QUANTIDADE, VALOR ) VALUES ( 6,'FILMADORA DIGITAL', 'DIGITAIS', 500, 700.00 );
+INSERT INTO PRODUTOS ( CODIGO, NOME, TIPO, QUANTIDADE, VALOR ) VALUES ( 7,'CELULAR', 'TELEFONE', 450, 850.00 ); 
+INSERT INTO PRODUTOS ( CODIGO, NOME, TIPO, QUANTIDADE, VALOR ) VALUES ( 8,'TECLADO', 'INFORMATICA', 300, 450.00 ); 
+INSERT INTO PRODUTOS ( CODIGO, NOME, TIPO, QUANTIDADE, VALOR ) VALUES ( 9,'VIDEOCASSETE', 'ELETRONICOS', 200, 300.00 ); 
+INSERT INTO PRODUTOS ( CODIGO, NOME, TIPO, QUANTIDADE, VALOR ) VALUES ( 10,'MOUSE', 'INFORMATICA', 400, 60.00 );
+
+show tables;
+
+desc PRODUTOS;
+
+-- Comandos select
+
+select * from produtos; 
+
+select nome,valor from produtos;
+
+select codigo as 'Código', nome as 'Nome do produto' from produtos;
+
+select distinct tipo from produtos;
+
+select * from produtos where quantidade > 250;
+
+select nome, tipo from produtos where valor <= 150;
+
+select * from produtos where quantidade >= 300 and valor <= 50;
+
+select * from produtos where quantidade >= 300 or valor <= 50;
+
+select * from produtos where nome = 'Monitor' and tipo = 'Informatica';
+
+select * from produtos where nome = 'Monitor' or tipo = 'Informatica';
+
+select * from produtos where tipo in('Informatica','Telefone');
+
